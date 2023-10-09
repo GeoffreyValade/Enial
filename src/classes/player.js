fb01d07cba8214aa8130667125a6472f8d32be73
Geoffrey Valade <geoffrey.valade@hotmail.com>
2023-10-09 15:51:14 +0200

    correction index

--- src/classes/player.js
+++ src/classes/player.js
@@ -1,20 +1,20 @@
-//import PlayersData from '../../datas/joueurs.json';
+//import playersData from '../../datas/joueurs.json';
 
 class Player {
-    constructor(PlayersData) {
-        this.joueur = PlayersData[index].joueur;
-        this.nom = PlayersData[index].nom;
-        this.prenom = PlayersData[index].prenom;
-        this.surnom = PlayersData[index].surnom;
-        this.age = PlayersData[index].age;
-        this.race = PlayersData[index].race;
-        this.sexe = PlayersData[index].sexe;
-        this.resumerp = PlayersData[index].resumerp;
-        this.description = PlayersData[index].description;
-        this.portrait = PlayersData[index].portrait;
-        this.caracsprincipales = PlayersData[index].caracs[0];
-        this.traits = PlayersData[index].traits;
-        this.domainesgenerauxJoueur = PlayersData[index].domainesgenerauxJoueur;
-        this.domainesmagiquesJoueur = PlayersData[index].domainesmagiquesJoueur;
+    constructor(playersData) {
+        this.joueur = playersData.joueur;
+        this.nom = playersData.nom;
+        this.prenom = playersData.prenom;
+        this.surnom = playersData.surnom;
+        this.age = playersData.age;
+        this.race = playersData.race;
+        this.sexe = playersData.sexe;
+        this.resumerp = playersData.resumerp;
+        this.description = playersData.description;
+        this.portrait = playersData.portrait;
+        this.caracsprincipales = playersData.caracs[0];
+        this.traits = playersData.traits;
+        this.domainesgenerauxJoueur = playersData.domainesgenerauxJoueur;
+        this.domainesmagiquesJoueur = playersData.domainesmagiquesJoueur;
     }
 }
