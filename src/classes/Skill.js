ef607c1de85f25fcebd2156485c1f37b94c9e8e7
Geoffrey Valade <geoffrey.valade@hotmail.com>
2023-10-11 09:54:20 +0200

    Dernières modifs

---
+++ src/classes/Skill.js
@@ -0,0 +1,10 @@
+class Skill {
+    constructor(label, type, cost, description, effects) {
+      // ...
+    }
+  }
+  
+  const skillType = {
+    Technique: "technique",
+    Sort: "sort",
+  }
--- src/components/Fiche/Feuilles/SortsEtTechniques/SortsEtTechniques.js
+++ src/components/Fiche/Feuilles/SortsEtTechniques/SortsEtTechniques.js
@@ -1,12 +1,46 @@
 import './SortsEtTechniques.css';
 
+
+
 export default function SortsEtTechniques({ prenom }) {
     return (
         <div className="sheet">
             <h1 className="sheet-title">Sorts et techniques</h1>
-            <ul>
-                <li>Prénom : {prenom}</li>
-            </ul>
+            <div className="newspell-form">
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="trait"></label>
+                        <input name="trait" />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="description"></label>
+                        <input name="description" />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="caracteristique"></label>
+                        <input name="trait" />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="caracteristique"></label>
+                        <input name="trait" />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="caracteristique"></label>
+                        <input name="trait" />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="caracteristique"></label>
+                        <input name="trait" />
+                    </div>
+
+                    <button type="submit" className="modal-btn">Envoyer</button>
+                </form>
+            </div>
         </div>
     );
 }
